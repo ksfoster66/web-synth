329a74bd3334cef2f3ae7a1f5c33814e12113ee8
ksfoster66 <ksfoster66@gmail.com>
2025-02-16 17:08:50 -0500

    Initial commit, basic setup andsome testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import * as Tone from 'tone';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [synths, setSynths] = useState([new Tone.Synth().toDestination(), new Tone.MetalSynth().toDestination()])
+
+  //Array of instruments
+  //active instrument
+  const [currentInstrument, setCurrentInstrument] = useState(0);
+
+  const playSound = () => {
+    if (Tone.Context.state !== "running"){
+      Tone.start()
+    }
+
+    synths[currentInstrument].triggerAttackRelease("C4", "8n");
+  };
+
+  const changeActiveInstrument = () => {
+    setCurrentInstrument(()=>{
+      let val = currentInstrument + 1;
+      if (val >= synths.length) val = 0;
+      return val;
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Web Synth</h1>
+        <div>Controls
+        <button id="play-btn" onClick={playSound}>play</button>
+        <button id="change-instrument" onClick={changeActiveInstrument}>switch</button>
+
+        </div>
+        <div>Instruments section</div>
+        <div>Midi section</div>
+        {/* Grid of buttons??
+        With preset note time durations */}
       </header>
     </div>
   );
