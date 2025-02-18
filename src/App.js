88b4ecc1df55db376ec623acd1c2db520a97796d
ksfoster66 <ksfoster66@gmail.com>
2025-02-17 21:01:33 -0500

    Validated logic of adding synths. Validated logic of adding effects to synths

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import './App.css';
 
 function App() {
-  const [synths, setSynths] = useState([new Tone.Synth().toDestination(), new Tone.MetalSynth().toDestination()])
+  const [synths, setSynths] = useState([[new Tone.Synth().toDestination()], [new Tone.MetalSynth().toDestination()]])
 
   //Array of instruments
   //active instrument
@@ -14,7 +14,10 @@ function App() {
       Tone.start()
     }
 
-    synths[currentInstrument].triggerAttackRelease("C4", "8n");
+    console.log(currentInstrument);
+    
+
+    synths[currentInstrument][0].triggerAttackRelease("C4", "8n");
   };
 
   const changeActiveInstrument = () => {
@@ -25,13 +28,46 @@ function App() {
     });
   }
 
+  const addInstrument = () => {//Add parameters to dictate what kind of synth to add
+    setSynths(()=> {
+      const newSynth = new Tone.DuoSynth().toDestination();
+
+      return [...synths, [newSynth]];
+    })
+  }
+
+  const addEffect = () => { //Add a parameter to dictate what type of effect to add
+    const effect = new Tone.FeedbackDelay("8n", 0.5);
+
+    let synthChain = synths[currentInstrument];
+    console.log(synthChain);
+    
+    console.log(synthChain[synthChain.length - 1]);
+
+    synthChain[synthChain.length - 1].disconnect();
+    synthChain[synthChain.length - 1].connect(effect);
+    effect.toDestination();
+
+    synthChain.push(effect);
+
+    setSynths(currentSynths=>{
+      const s = [...currentSynths];
+      s[currentSynths] = [...synthChain];
+      return s;
+    })
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Web Synth</h1>
-        <div>Controls
+        <div>
+          <h3>Controls</h3>
         <button id="play-btn" onClick={playSound}>play</button>
         <button id="change-instrument" onClick={changeActiveInstrument}>switch</button>
+        <button id="add-instrument" onClick={addInstrument}>add</button>
+        <button id="add-effect" onClick={addEffect}>effect</button>
 
         </div>
         <div>Instruments section</div>
